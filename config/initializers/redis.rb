17cce41f06b3831dbc1fc663aed1fd1de58e2c36
Jason Jordan <jtjordan13@gmail.com>
2022-05-23 12:26:52 -0400

    heroku redis ahoy!

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-Redis.current = Redis.new(url:  ENV['REDIS_URL'],
-                          port: ENV['REDIS_PORT'],
-                          db:   ENV['REDIS_DB'])
+# Redis.current = Redis.new(url:  ENV['REDIS_URL'],
+#                           port: ENV['REDIS_PORT'].to_i,
+#                           db:   ENV['REDIS_DB'].to_i)
+
+Redis.current = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
