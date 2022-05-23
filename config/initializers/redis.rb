75f9f78c2c13b66c1af5d5d7b8beac6bac834fd3
Jason Jordan <jtjordan13@gmail.com>
2022-05-23 14:06:03 -0400

    heroku redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,4 @@
 #                           port: ENV['REDIS_PORT'].to_i,
 #                           db:   ENV['REDIS_DB'].to_i)
 
-Redis.current = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+Redis.current = Redis.new(url: ENV["REDISTOGO_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
